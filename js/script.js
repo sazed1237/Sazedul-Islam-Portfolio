0407702f5eef91bbcbbe2ee2a047e0d4d9e9d557
sazed1237 <sazedulislam9126@gmail.com>
2024-04-06 03:34:32 +0600

    navbar problem solved

--- js/script.js
+++ js/script.js
@@ -1,45 +1,22 @@
-$(document).ready(function() {
-    $.ajax({
-        url: "https://example.com/api/data",
-        method: "GET",
-        success: function(response) {
-            console.log(response);
-        },
-        error: function(xhr, status, error) {
-            console.log("AJAX request failed: " + error);
-        }
-    });
-});
+
+
+// stricky header
+let header = document.querySelector('header');
+window.addEventListener('scroll', function () {
+    header.classList.toggle('sticky', window.scrollY > 100);
+})
 
 
 
+let menu = document.querySelector('#menu-icon');
+let navList = document.querySelector('.navbar');
 
-// scroll sections
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navList.classList.toggle('open');
+};
 
 window.onscroll = () => {
-
-    sections.forEach(sec =>{
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 100;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        if(top >= offset && top < offset + height){
-            // active navbar links
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-        }
-    })
-
-    // stricky header
-    let header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 100);
+    menu.classList.remove('bx-x');
+    navList.classList.remove('open');
 }
-
-
